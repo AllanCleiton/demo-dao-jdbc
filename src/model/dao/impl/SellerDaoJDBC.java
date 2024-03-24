e3a85187a00cdf57c8956cae9114fce2dc94ee0f
Allan Cleiton <allancleitonppma@gmail.com>
2024-03-24 13:49:13 -0300

    FindById implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,15 +1,50 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbIntegretyException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao{
+	
+	private Connection conn;
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
 
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement ps = null;
+		
+		try {
+			conn = DB.getConnection();
+			ps = conn.prepareStatement("INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId) VALUES (?, ?, ?, ?, ?)");
+			ps.setString(1, obj.getName());
+			ps.setString(2, obj.getEmail());
+			ps.setDate(3, (Date) obj.getBirthDate());
+			ps.setDouble(4, obj.getBaseSalary());
+			ps.setInt(5, obj.getDepartment().getId());
+			
+			int rowsAffected = ps.executeUpdate();
+			System.out.println("Rows affected = " + rowsAffected);
+			
+		}
+ 		catch(SQLException e) {
+			throw new DbIntegretyException(e.getMessage());
+		}
+		finally {
+			DB.closeConnection();
+			DB.closeStatement(ps);
+		}
+		
 		
 	}
 
@@ -27,8 +62,47 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public Seller findbyID(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Seller seller = null;
+		
+		try {
+			conn = DB.getConnection();
+			ps = conn.prepareStatement(
+					"SELECT seller.*, department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE seller.id = ?");
+			ps.setInt(1, id);
+			rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				Department dep = new Department();
+				dep.setId(rs.getInt("DepartmentId"));
+				dep.setName(rs.getString("DepName"));
+				
+				Seller obj = new Seller();
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				obj.setEmail(rs.getString("Email"));
+				obj.setBaseSalary(rs.getDouble("BaseSalary"));
+				obj.setBirthDate(rs.getDate("BirthDate"));
+				obj.setDepartment(dep);
+				return obj;
+			}
+			
+			return null;
+			
+			
+		}
+ 		catch(SQLException e) {
+			throw new DbIntegretyException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(ps);
+			DB.closeResultSet(rs);
+		}
+	
 	}
 
 	@Override
